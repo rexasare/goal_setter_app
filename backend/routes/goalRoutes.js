86642ab57d9552ff38a546c7b53f3344b1242d05
Rex Asare <rex.asare06@gmail.com>
2022-06-19 19:57:30 +0000

    created goals api resource

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getGoals,
+  setGoal,
+  deleteGoal,
+  updateGoal,
+} = require("../controllers/goalController");
+
+//router groups
+router.route("/").get(getGoals).post(setGoal);
+router.route("/:id").delete(deleteGoal).put(updateGoal);
+
+module.exports = router;
